44028b1d10de1e97f5513a6ff8f3a286ec3f6f4e
Pierre Gerges <pgerges@gpo.gov>
2022-10-07 16:22:54 -0400

    Create Class ApplicationDbContext Add Constructor to ApplicationDbContext Configure EntityFramework Create Item Model Add Item to ApplicationDbContext

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        // Create a constructor and invoke the base (DbContext) constructor 
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+    }
+}
